45f8b29c27de0708bed852509dca023027a6e8d9
jkim000 <joshuakim514@gmail.com>
2021-03-30 17:29:51 -0700

    fix navbar Link Router

--- src/Client/components/NavBar.tsx
+++ src/Client/components/NavBar.tsx
@@ -1,10 +1,12 @@
 import React, { useContext, useEffect, useState } from "react";
+import { Link } from 'react-router-dom';
 import {StateContext} from "../provider/StateProvider";
 
 const NavBar = () => {
   return (
     <div>
       <ul id='nav'>
+        <Link to="/userinfo"></Link>
         <li>Username</li>
         <li><Link to='/savedPallette'>Saved Color Pallet</Link></li>
         <li>Color Picker</li>
