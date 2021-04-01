20e8acdff7649ad85e988e6ca79dfcf506b8bf1b
Abu <bubakarrr@gmail.com>
2021-04-01 13:53:19 -0400

    added auth controller to get all info for user

--- src/Server/routes/authRouter.js
+++ src/Server/routes/authRouter.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const router = express.Router();
-
+const cookieParser = require('cookie-parser');
 const authController = require('../controllers/authController.js');
+router.use(cookieParser());
+router.post(
+  '/createUser',
+  authController.createUser,
+  authController.getInfo,
+  (req, res) => {
+    res.cookie('email', res.locals.info.email);
+    res.status(200).json(res.locals.all);
+  }
+);
 
-router.post('/createUser', authController.createUser, (req,res)=>{
-    res.status(200).json(res.locals.info)
-})
-
-
-
+router.post(
+  '/getUser',
+  authController.validateUser,
+  authController.getInfo,
+  (req, res) => {
+    res.cookie('email', res.locals.info.email);
+    console.log('cookie stuff backend----', req.cookie, res.cookie);
+    res.status(200).json(res.locals.all);
+  }
+);
 module.exports = router;
--- src/Server/server.js
+++ src/Server/server.js
@@ -11,16 +11,6 @@ const bodyParser = require('body-parser');
 // const passport = require('passport');
 const cookieSession = require('cookie-session');
 require('./passport-setup');
-// app.use(cors());
-
-// parse application/x-www-form-urlencoded
-// app.use(bodyParser.urlencoded({ extended: false }));
-
-// parse application/json
-// app.use(bodyParser.json());
-
-// app.use(passport.initialize());
-// app.use(passport.session());
 
 const PORT = 8000;
 
@@ -37,30 +27,26 @@ app.use('/palette', paletteRouter);
 app.use('/auth', authRouter);
 // app.use('/user', userRouter);
 
-
 app.get('/newUser', (req, res) => {
   res.setHeader('Content-type', 'text/html');
   res.status(200).sendFile(path.resolve(__dirname, '../client/signup.html'));
 });
-
 
 app.get('/home', (req, res) => {
   res.setHeader('Content-type', 'text/html');
   res.status(200).sendFile(path.resolve(__dirname, '../client/index.html'));
 });
 
-
 app.get('/', (req, res) => {
   res.setHeader('Content-type', 'text/html');
   res.status(200).sendFile(path.resolve(__dirname, '../client/signin.html'));
 });
 
-
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
 // global error handler
 app.use((err, req, res, next) => {
-  console.log("Global error", err);
+  console.log('Global error', err);
   res.status(500).json(err);
 });
 
