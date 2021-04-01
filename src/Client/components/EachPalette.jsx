62603531bb79f733b30864ae2e90c90538ed6745
Jason S <jcspeare@gmail.com>
2021-04-01 11:46:58 -0700

    Updated ColorPalette to functional component with hooks

--- src/Client/components/EachPalette.jsx
+++ src/Client/components/EachPalette.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { StateContext } from "../provider/StateProvider";
 
 
 const EachPalette = ({_id, paletteName, email_fk, palette}) => {
+  const [collection, setCollection] = useState([]);
+
   const handleDelete = () => {
     fetch(`/palette/delete/${paletteName}`, {
       method: 'DELETE'
     })
-    // .then(window.location.reload(false))
+    .then(res => {
+      setCollection(res)
+    })
   }
 
   const setOfPalettes = palette.map((ele, i) => (
